2d85260a2aa0a0664eccca3ae3e970f9d3a4702f
EBorzov <borzov-nv@ya.ru>
2026-01-06 02:07:44 +0500

    1

--- vm.tf
+++ vm.tf
@@ -143,6 +143,88 @@ resource "yandex_compute_instance" "elasticsearch" {
   }
 }
 
+resource "yandex_alb_target_group" "web_targets" {
+  name = "web-target-group"
+
+  dynamic "target" {
+    for_each = yandex_compute_instance.web
+    content {
+      subnet_id  = target.value.network_interface.0.subnet_id
+      ip_address = target.value.network_interface.0.ip_address
+    }
+  }
+}
+
+resource "yandex_alb_backend_group" "web_backends" {
+  name = "web-backend-group"
+
+  http_backend {
+    name             = "http-backend"
+    weight           = 1
+    port             = 80
+    target_group_ids = [yandex_alb_target_group.web_targets.id]
+    
+    load_balancing_config {
+      panic_threshold = 50
+    }    
+    healthcheck {
+      timeout             = "1s"
+      interval            = "1s"
+      http_healthcheck {
+        path = "/"
+      }
+    }
+  }
+}
+
+resource "yandex_alb_http_router" "web_router" {
+  name = "web-http-router"
+}
+
+resource "yandex_alb_virtual_host" "web_vhost" {
+  name           = "web-virtual-host"
+  http_router_id = yandex_alb_http_router.web_router.id
+  route {
+    name = "root-route"
+    http_route {
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.web_backends.id
+        timeout          = "3s"
+      }
+    }
+  }
+}
+
+resource "yandex_alb_load_balancer" "main_alb" {
+  name               = "main-load-balancer"
+  network_id         = yandex_vpc_network.main.id
+  security_group_ids = [yandex_vpc_security_group.alb_sg.id]
+
+  allocation_policy {
+    location {
+      zone_id   = "ru-central1-a"
+      subnet_id = yandex_vpc_subnet.public.id
+    }
+  }
+
+  listener {
+    name = "web-listener"
+    endpoint {
+      address {
+        external_ipv4_address {
+          # This will provide a public IP automatically
+        }
+      }
+      ports = [80]
+    }
+    http {
+      handler {
+        http_router_id = yandex_alb_http_router.web_router.id
+      }
+    }
+  }
+}
+
 resource "local_file" "ansible_inventory" {
   content = templatefile("hosts.tftpl",
     {
@@ -153,4 +235,21 @@ resource "local_file" "ansible_inventory" {
     }
   )
   filename = "ansible/hosts.ini" 
+}
+
+output "bastion_public_ip" {
+  description = "Connect to this IP for SSH/Ansible management"
+  value       = yandex_compute_instance.bastion.network_interface.0.nat_ip_address
+}
+
+output "web_site_url" {
+  description = "Visit this IP in your browser to see your website"
+  value       = "http://${yandex_alb_load_balancer.main_alb.listener.0.endpoint.0.address.0.external_ipv4_address.0.address}"
+}
+
+output "monitoring_ips" {
+  description = "Public IPs for Zabbix and Kibana"
+  value = {
+    for k, v in yandex_compute_instance.monitoring : k => v.network_interface.0.nat_ip_address
+  }
 }
